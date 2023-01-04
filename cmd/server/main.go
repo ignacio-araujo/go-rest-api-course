5d69f79d50347f8eed823266bb29e179c160eabe
Ignacio Araújo <ignacio.araujo@fing.edu.uy>
2023-01-04 14:41:59 -0300

    Structuring the app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+//App struct which contains pointers to database connections
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up Our APP")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
